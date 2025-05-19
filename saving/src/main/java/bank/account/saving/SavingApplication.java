bdaf0c701ec88f1da02f0e4566d899569983f117
Chatchakan Yu-on <chatchakanyuon@gmail.com>
2025-05-19 15:56:33 +0700

    feat: Auto migration default teller

--- saving/src/main/java/bank/account/saving/SavingApplication.java
+++ saving/src/main/java/bank/account/saving/SavingApplication.java
@@ -1,6 +1,8 @@
 package bank.account.saving;
 
+import bank.account.saving.model.Customer;
 import bank.account.saving.model.Stakeholder;
+import bank.account.saving.repository.CustomerRepository;
 import bank.account.saving.repository.StakeholderRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,24 +12,38 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SavingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SavingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SavingApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(StakeholderRepository stakeholderRepository) {
-		return args -> {
-			if (stakeholderRepository.findByName("CUSTOMER").isEmpty()) {
-				stakeholderRepository.save(Stakeholder.builder()
-						.name("CUSTOMER")
-						.build());
-			}
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            StakeholderRepository stakeholderRepository,
+            CustomerRepository customerRepository
+    ) {
+        return args -> {
+            if (stakeholderRepository.findByName("CUSTOMER").isEmpty()) {
+                stakeholderRepository.save(Stakeholder.builder()
+                        .name("CUSTOMER")
+                        .build());
+            }
 
-			if (stakeholderRepository.findByName("TELLER").isEmpty()) {
-				stakeholderRepository.save(Stakeholder.builder()
-						.name("TELLER")
-						.build());
-			}
-		};
-	}
+            if (stakeholderRepository.findByName("TELLER").isEmpty()) {
+                stakeholderRepository.save(Stakeholder.builder()
+                        .name("TELLER")
+                        .build());
+            }
+
+            if (customerRepository.findByEmail("teller@mail.com").isEmpty()) {
+                customerRepository.save(Customer.builder()
+                        .email("teller@mail.com")
+                        .password("teller_password")
+                        .cid("111111111111")
+                        .thaiName("เทลเลอร์")
+                        .englishName("Teller")
+                        .pin("111111")
+                        .build());
+            }
+        };
+    }
 }
